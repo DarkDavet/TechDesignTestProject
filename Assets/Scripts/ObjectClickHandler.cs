f1e8c9fc57f0798943ebc9a1039f19b5a4e713de
DarkDavet <darkdavet@gmail.com>
2025-03-23 16:05:48 +0300

    сompleted the main work on level_1_scene: added game objects, VFX and animations.

---
+++ Assets/Scripts/ObjectClickHandler.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectClickHandler : MonoBehaviour
+{
+    [SerializeField] private GameObject particlePrefab;
+    [SerializeField] private bool isLoop;
+    [SerializeField] private string animationName;
+    private bool isVFXPlay;
+    private Animator animator;
+
+    private GameObject particleInstance;
+    private ParticleSystem particle;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        if (particlePrefab == null)
+        {
+            Debug.LogError("Particle prefab is not assigned!");
+        }
+
+        isVFXPlay = false;
+    }
+
+    private void OnMouseDown()
+    {
+        PlayAnimation();
+        PlayVFX();
+    }
+
+    private void PlayAnimation()
+    {
+        if (animator != null)
+        {
+            animator.Play(animationName, -1, 0f);
+        }
+    }
+
+    private void PlayVFX()
+    {
+        if (particlePrefab != null && isVFXPlay == false)
+        {
+            particleInstance = Instantiate(particlePrefab, transform.position, Quaternion.identity);
+            particle = particleInstance.GetComponent<ParticleSystem>();
+            if (particle != null && isLoop == false)
+            {
+                particle.Play();
+
+                Destroy(particleInstance, particle.main.duration + particle.main.startLifetime.constantMax);
+            }
+            else if (particlePrefab != null && isLoop == true)
+            {
+                isVFXPlay = true;
+                particle.Play();
+            }
+        }
+
+        else if (isVFXPlay)
+        {
+            Destroy(particleInstance);
+            particle = null;
+            isVFXPlay = false;
+        }
+    }
+}
